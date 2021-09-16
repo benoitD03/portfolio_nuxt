b5f8c1037eb67fda10ec4fe5c97e63470e9ecbb9
benoitD03 <bens.92@hotmail.fr>
2021-09-16 11:03:44 +0200

    add realisations page

--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,39 @@
     <h1><span>B</span>enoit Durand</h1>
     <h2><span>D</span>éveloppeur Frontend</h2>
     <div class="mt-5">
-      <v-btn class="py-12 px-16" elevation="2"><b>Compétences</b></v-btn>
+      
+      <v-col cols="auto">
+      <v-dialog
+        transition="dialog-bottom-transition"
+        max-width="600"
+      >
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn
+            v-bind="attrs"
+            v-on="on"
+            class="py-12 px-16"
+          ><b>Compétences</b></v-btn>
+        </template>
+        <template v-slot:default="dialog">
+          <v-card>
+            <v-toolbar
+              color="primary"
+              dark
+            >Opening from the bottom</v-toolbar>
+            <v-card-text>
+              <div class="text-h2 pa-12">Hello world!</div>
+            </v-card-text>
+            <v-card-actions class="justify-end">
+              <v-btn
+                text
+                @click="dialog.value = false"
+              >Close</v-btn>
+            </v-card-actions>
+          </v-card>
+        </template>
+      </v-dialog>
+    </v-col>
+
       <NuxtLink to="/realisations">
         <v-btn class="py-12 px-16" elevation="2"><b>Réalisations</b></v-btn>
       </NuxtLink>
--- pages/realisations.vue
+++ pages/realisations.vue
@@ -1,13 +1,53 @@
 <template>
-  <h1>Page de mes Réalisations !</h1>
+  <div>
+    <h1>Page de mes Réalisations !</h1>
+    <v-carousel v-model="model">
+      <v-carousel-item v-for="(project, id) in projects" :key="id">
+        <v-sheet :project="project" height="100%" tile>
+          <v-row class="fill-height" align="center" justify="center">
+            <div class="text-h2">Projet {{ id + 1 }}</div>
+            <img align="center" :src="project.image" :alt="project.title" />
+            <NuxtLink to="/">
+              <v-btn v-bind="attrs" v-on="on" class="py-12 px-16"
+                ><b>Voir le projet</b></v-btn
+              >
+            </NuxtLink>
+          </v-row>
+        </v-sheet>
+      </v-carousel-item>
+    </v-carousel>
+  </div>
 </template>
 
 <script>
 export default {
-
-}
+  async asyncData() {
+    return {
+      projects: [
+        {
+          id: 1,
+          title: "Frontend e-commerce + interface administrateur",
+          description:
+            "Réalisation d'un site type e-commerce : développement d'une partie administrateur permettant d'ajouter, modifier et supprimer des articles, et développement de la partie client, avec les fonctionnalités d'ajout et de suppression d'articles dans un panier.",
+          image: "/image_ecommerce.png",
+          technos: ["Vue.js", "Vuex", "Json-server", "localStorage"],
+        },
+        {
+          id: 2,
+          title: "Réseau social d'entreprise",
+          description:
+            "Réalisation d'un réseau social, avec la gestion des utilisateurs ( connexion, inscription, modifications et suppressions de profil, possibilité aux admin de supprimer d'autres utilisateurs, etc ), et partage de posts sur un fil d'actualité ( avec également la possibilité aux admin de supprimer tous les posts des utilisateurs )",
+          image: "/image_groupomania.png",
+          technos: ["Vue.js", "Vuetify", "Node.js", "MySQL", "Sequelize"],
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <style>
-
+img {
+  width: 500px;
+}
 </style>
