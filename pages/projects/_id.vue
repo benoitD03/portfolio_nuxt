addac5b40b8f0ca153ae7db5d96b62f93ffd8e0e
benoitD03 <bens.92@hotmail.fr>
2021-09-17 12:33:47 +0200

    modify css

--- pages/projects/_id.vue
+++ pages/projects/_id.vue
@@ -1,33 +1,75 @@
 <template>
   <div>
-      <h1>{{ project.title }}</h1>
-      <img :src="project.image" alt="project.title">
+    <h1 class="text-center my-15">{{ project.title }}</h1>
+    <v-row>
+      <v-col cols="12" lg="6" sm="12" md="12">
+        <img :src="project.image" alt="project.title" />
+      </v-col>
+      <v-col cols="12" lg="6" md="12" sm="12">
+        <img :src="project.image" alt="project.title" />
+      </v-col>
+    </v-row>
+    <v-divider class="mt-9"></v-divider>
+    <div class="text-container mt-9">
+      <h3 class="text-center mb-9"><span>D</span>escription</h3>
       <p>{{ project.description }}</p>
-      <v-btn v-for="techno in project.technos" :key="techno.index">{{ techno }}</v-btn>
+      <v-divider class="mt-9"></v-divider>
+      <h3 class="text-center my-9"><span>S</span>tack technique</h3>
+      <div class="techno"> 
+      <span class="mr-3" v-for="techno in project.technos" :key="techno.index">
+          <b>{{techno}}</b>    
+      </span>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-data() {
+  data() {
     return {
-        project: {
-            id: '',
-            title: '',
-            image: '',
-            decription: '',
-            technos: []
-        }
-    }
-},
-async asyncData({ store, route }) {
+      project: {
+        id: "",
+        title: "",
+        image: "",
+        decription: "",
+        technos: [],
+      },
+    };
+  },
+  async asyncData({ store, route }) {
     return {
-        project: store.getters.getProjects.find(project => project.id === route.params.id)
-    }
-}
-}
+      project: store.getters.getProjects.find(
+        (project) => project.id === route.params.id
+      ),
+    };
+  },
+};
 </script>
 
-<style>
-
+<style scoped>
+h1 {
+  font-size: 40px;
+  background: #3ebfa6;
+  width: 100%;
+  border-radius: 10px;
+}
+h3 {
+    font-size: 30px;
+}
+img {
+  width: 500px;
+  height: auto;
+  border-radius: 10px;
+}
+.project-container {
+  display: flex;
+}
+span {
+    color: #3ebfa6;
+}
+.techno {
+    display: flex;
+    justify-content: center;
+}
 </style>
--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -1,18 +1,17 @@
 <template>
   <div>
-    <h1>Page de mes Réalisations !</h1>
-    <v-carousel v-model="model">
+    <h1 class="text-center my-9"><span>R</span>éalisations</h1>
+    <v-carousel style="border-radius: 10px;" height="500" hide-delimiter-background show-arrows-on-hover>
       <v-carousel-item v-for="(project, id) in projects" :key="id">
         <v-sheet :project="project" height="100%" tile>
-          <v-row class="fill-height" align="center" justify="center">
-            <div class="text-h2">Projet {{ id + 1 }}</div>
-            <img align="center" :src="project.image" :alt="project.title" />
-            <NuxtLink :to="{name: 'projects-id', params: { id: project.id}}">
-              <v-btn v-bind="attrs" v-on="on" class="py-12 px-16"
-                ><b>Voir le projet</b></v-btn
-              >
+          
+            <h3 class="text-center my-6">{{ project.title }}</h3>
+            <div class="caroussel-content">
+            <NuxtLink align="center" class="text-center" :to="{name: 'projects-id', params: { id: project.id}}">
+              <img class="mt-10" :src="project.image" :alt="project.title" />  
             </NuxtLink>
-          </v-row>
+            </div>
+          
         </v-sheet>
       </v-carousel-item>
     </v-carousel>
@@ -29,8 +28,42 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
+a {
+  width: 70%;
+}
 img {
-  width: 500px;
+  border-radius: 10px;
+  width: 70%;
+  -webkit-transition: all 1s ease; /* Safari et Chrome */
+  -moz-transition: all 1s ease; /* Firefox */
+  -ms-transition: all 1s ease; /* Internet Explorer 9 */
+  -o-transition: all 1s ease; /* Opera */
+  transition: all 1s ease;
+}
+img:hover {
+  -webkit-transform:scale(1.25); /* Safari et Chrome */
+  -moz-transform:scale(1.25); /* Firefox */
+  -ms-transform:scale(1.25); /* Internet Explorer 9 */
+  -o-transform:scale(1.25); /* Opera */
+  transform:scale(1.25);
+}
+h3 {
+  background: #3ebfa6;
+  font-size: 30px;
+  border-radius: 10px 10px 0 0 ;
+}
+h1 {
+  font-size: 45px;
+}
+span {
+  color: #3ebfa6;
+}
+.caroussel-content {
+  display: flex;
+  justify-content: center;
+}
+.v-carousel__controls {
+  background: #3ebfa6 !important;
 }
 </style>
